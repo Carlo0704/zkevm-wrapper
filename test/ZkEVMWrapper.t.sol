6219ec7b7f880d87ffeeb030b61f70e0041febc9
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2024-04-09 17:54:04 +0530

    feat: support dynamic destination network

--- test/ZkEVMWrapper.t.sol
+++ test/ZkEVMWrapper.t.sol
@@ -22,21 +22,32 @@ contract ZkEVMWrapperTest is Test {
     function setUp() external {
         token = new MockERC20();
         dai = new MockDai(block.chainid);
-        sigUtils = new SigUtils(IERC20Permit(token).DOMAIN_SEPARATOR(), dai.DOMAIN_SEPARATOR());
+        sigUtils = new SigUtils(
+            IERC20Permit(token).DOMAIN_SEPARATOR(),
+            dai.DOMAIN_SEPARATOR()
+        );
         bridge = new MockZkEVMBridge();
         wrapper = new ZkEVMWrapper(IZkEVMBridge(address(bridge)));
     }
 
-    function testDeposit(address user, uint256 tokenAmount, uint256 etherAmount, address destination)
-        external
-        payable
-    {
+    function testDeposit(
+        address user,
+        uint32 destinationNetwork,
+        uint256 tokenAmount,
+        uint256 etherAmount,
+        address destination
+    ) external payable {
         vm.assume(user != address(0) && etherAmount != 0);
         vm.deal(user, etherAmount);
         token.mint(user, tokenAmount);
         vm.startPrank(user);
         token.approve(address(wrapper), tokenAmount);
-        wrapper.deposit{value: etherAmount}(IERC20(token), tokenAmount, destination);
+        wrapper.deposit{value: etherAmount}(
+            IERC20(token),
+            destinationNetwork,
+            destination,
+            tokenAmount
+        );
         assertEq(token.balanceOf(address(bridge)), tokenAmount);
         assertEq(address(bridge).balance, etherAmount);
     }
@@ -50,18 +61,34 @@ contract ZkEVMWrapperTest is Test {
     ) external payable {
         // privkey value must be lower than secp256k1 curve order
         vm.assume(
-            privKey != 0 && privKey < 115792089237316195423570985008687907852837564279074904382605163141518161494337
+            privKey != 0 &&
+                privKey <
+                115792089237316195423570985008687907852837564279074904382605163141518161494337
         );
         address user = vm.addr(privKey);
         vm.assume(user != address(0) && deadline > 0 && etherAmount != 0);
         vm.deal(user, etherAmount);
         token.mint(user, tokenAmount);
         vm.startPrank(user);
-        SigUtils.Permit memory permit =
-            SigUtils.Permit({owner: user, spender: address(wrapper), value: tokenAmount, nonce: 0, deadline: deadline});
+        SigUtils.Permit memory permit = SigUtils.Permit({
+            owner: user,
+            spender: address(wrapper),
+            value: tokenAmount,
+            nonce: 0,
+            deadline: deadline
+        });
         bytes32 digest = sigUtils.getTypedDataHash(permit);
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(privKey, digest);
-        wrapper.deposit{value: etherAmount}(IERC20(token), tokenAmount, destination, deadline, v, r, s);
+        wrapper.deposit{value: etherAmount}(
+            IERC20(token),
+            1,
+            destination,
+            tokenAmount,
+            deadline,
+            v,
+            r,
+            s
+        );
         assertEq(token.balanceOf(address(bridge)), tokenAmount);
         assertEq(address(bridge).balance, etherAmount);
     }
@@ -75,18 +102,36 @@ contract ZkEVMWrapperTest is Test {
     ) external payable {
         // privkey value must be lower than secp256k1 curve order
         vm.assume(
-            privKey != 0 && privKey < 115792089237316195423570985008687907852837564279074904382605163141518161494337
+            privKey != 0 &&
+                privKey <
+                115792089237316195423570985008687907852837564279074904382605163141518161494337
         );
         address user = vm.addr(privKey);
         vm.assume(user != address(0) && expiry > 0 && etherAmount != 0);
         vm.deal(user, etherAmount);
         dai.mint(user, tokenAmount);
         vm.startPrank(user);
-        SigUtils.DaiPermit memory permit =
-            SigUtils.DaiPermit({holder: user, spender: address(wrapper), nonce: 0, expiry: expiry, allowed: true});
+        SigUtils.DaiPermit memory permit = SigUtils.DaiPermit({
+            holder: user,
+            spender: address(wrapper),
+            nonce: 0,
+            expiry: expiry,
+            allowed: true
+        });
         bytes32 digest = sigUtils.getDaiTypedDataHash(permit);
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(privKey, digest);
-        wrapper.deposit{value: etherAmount}(IDai(address(dai)), tokenAmount, destination, 0, expiry, true, v, r, s);
+        wrapper.deposit{value: etherAmount}(
+            IDai(address(dai)),
+            tokenAmount,
+            1,
+            destination,
+            0,
+            expiry,
+            true,
+            v,
+            r,
+            s
+        );
         assertEq(dai.balanceOf(address(bridge)), tokenAmount);
         assertEq(address(bridge).balance, etherAmount);
     }
